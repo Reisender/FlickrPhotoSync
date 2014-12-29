f5fdd0cba2aff96f89cae65a697428632828e887
Doug Weber <doug@masteryconnect.com>
2014-12-28 22:09:53 -0700

    actually parse the config path cmd line arg

--- syncphotos/main.go
+++ syncphotos/main.go
@@ -7,9 +7,12 @@ import (
 	"github.com/Reisender/photosync"
 )
 
-var configPath = flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
 
 func main() {
+	configPath := flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
+
+	flag.Parse()
+
 	if err := photosync.LoadConfig(configPath); err != nil {
 		log.Fatalf("Error reading configuration, %v", err)
 	}
