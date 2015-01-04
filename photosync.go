830a6025cfc58d8e96c7420cfc81ff80ffde125f
Doug Weber <doug@masteryconnect.com>
2015-01-03 22:46:37 -0700

    used parsed date from filename in all cases as date taken.

--- photosync.go
+++ photosync.go
@@ -102,6 +102,16 @@ func Sync(api *FlickrAPI, photos *PhotosMap, dryrun bool) (int, int, error) {
 	return existingCount, uploadedCount, nil
 }
 
+func getTimeFromTitle(api *FlickrAPI, title string) (*time.Time, error) {
+	for _, tf := range api.config.FilenameTimeFormats {
+		t, err := time.Parse(tf, title)
+		if err == nil {
+			return &t, nil
+		}
+	}
+	return nil, Error{"no timestamp in title"}
+}
+
 //
 // Checks the EXIF data for JPGs and returns the path to either the original or the fixed JPG file.
 // The 2nd return value should be called when use of the JPG is complete.
@@ -111,16 +121,40 @@ func Sync(api *FlickrAPI, photos *PhotosMap, dryrun bool) (int, int, error) {
 func FixExif(title string, path string, f os.FileInfo, err error) (string, func(api *FlickrAPI, photoId string), error) {
 	ext := filepath.Ext(f.Name())
 	extUpper := strings.ToUpper(ext)
-	noop := func(api *FlickrAPI, photoId string) {}
+	var timeFromFilename *time.Time
+
+	_setDateTaken := func(api *FlickrAPI, photoId string) {
+		var err error
+		timeFromFilename, err = getTimeFromTitle(api, title)
+		if err != nil { timeFromFilename = nil }
+
+		// check the file name
+		if timeFromFilename != nil {
+			fmt.Printf("set time from file name: %s\n",timeFromFilename.Format(flickrTimeLayout))
+			api.SetDate(photoId, timeFromFilename.Format(flickrTimeLayout)) // eat the error as this is optional
+		}
+	}
+
+	_setDateTakenMov := func(api *FlickrAPI, photoId string) {
+		// they are done uploading the file so let's set it's date
+		_setDateTaken(api, photoId)
+
+		if timeFromFilename == nil {
+			// fall back to the mod time
+			// we do this for MOV's because there isn't exif data to use
+			fmt.Printf("set time to: %s\n",f.ModTime().Format(flickrTimeLayout))
+			api.SetDate(photoId, f.ModTime().Format(flickrTimeLayout)) // eat the error as this is optional
+		}
+	}
 
 
 	if extUpper == ".JPG" {
 		// check for valid exif data
 		out, err := exec.Command("exiftool","-a","-u","-g1","-json",path).CombinedOutput()
-		if err != nil { return "", noop, err }
+		if err != nil { return "", _setDateTaken, err }
 
 		exif := make([]exifToolOutput,1)
-		if err := json.Unmarshal(out, &exif); err != nil { return "", noop, err }
+		if err := json.Unmarshal(out, &exif); err != nil { return "", _setDateTaken, err }
 
 		if len(exif[0].ExifTool.Warning) > 0 {
 			// we have an exif error
@@ -129,40 +163,23 @@ func FixExif(title string, path string, f os.FileInfo, err error) (string, func(
 
 				// create tmp file and copy
 				tmpfile, err := ioutil.TempFile("",f.Name()+".")
-				if err != nil { return "", noop, err }
+				if err != nil { return "", _setDateTaken, err }
 
 				tmpfilePath := tmpfile.Name() // ensure it's a new file for the sake of
 				os.Remove(tmpfile.Name())
 
 				_, errr := exec.Command("exiftool","-exif:all=","-tagsfromfile","@","-all:all","-unsafe","-o",tmpfilePath,path).CombinedOutput()
-				if errr != nil { return "", noop, errr }
+				if errr != nil { return "", _setDateTaken, errr }
 
 				// return the callback function that should get called when use of this image is complete
 				return tmpfilePath, func(api *FlickrAPI, photoId string) {os.Remove(tmpfilePath) }, errr
 			}
 		}
+
 	} else if extUpper == ".MOV" || extUpper == ".MP4" {
 		// always set to the file's modified date
-		return path, func(api *FlickrAPI, photoId string) {
-			// they are done uploading the file so let's set it's date
-
-			// start with the file name
-			for _, tf := range api.config.FilenameTimeFormats {
-				t, err := time.Parse(tf, title)
-				if err == nil {
-					// yessss... use it
-					fmt.Printf("set time from file name: %s\n",t.Format(flickrTimeLayout))
-					api.SetDate(photoId, f.ModTime().Format(flickrTimeLayout)) // eat the error as this is optional
-					return // we are done
-				}
-			}
-
-			// fall back to the mod time
-			fmt.Printf("set time to: %s\n",f.ModTime().Format(flickrTimeLayout))
-			api.SetDate(photoId, f.ModTime().Format(flickrTimeLayout)) // eat the error as this is optional
-
-		}, nil
+		return path, _setDateTakenMov, nil
 	}
 
-	return path, noop, nil
+	return path, _setDateTaken, nil
 }
